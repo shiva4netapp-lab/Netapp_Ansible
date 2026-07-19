2d624892e3915fa3c4f0cc572076136f68fb4830
shiva4netapp-lab <shiva4netapp@gmail.com>
2026-07-19 14:27:08 +0530

    Add functions to show errors and all rows

--- templates/js/report.js
+++ templates/js/report.js
@@ -1,1 +1,21 @@
+function showErrorsOnly() {
 
+    let rows=document.querySelectorAll(".check-row");
+
+    rows.forEach(function(row){
+
+        if(row.dataset.error==="false"){
+            row.style.display="none";
+        }
+
+    });
+}
+
+function showAll() {
+
+    let rows=document.querySelectorAll(".check-row");
+
+    rows.forEach(function(row){
+        row.style.display="";
+    });
+}
